fee8216cbcc9fae13dca790ce4a2f0e6cf606e4d
Malay <malaykbasu@gmail.com>
2016-05-16 10:03:44 -0500

    Added getlast.sh

--- getlast.sh
+++ getlast.sh
@@ -1,3 +1,4 @@
+# Wed Apr 27 10:44:54 CDT 2016
 # This is a function to get the last command from the history and print it to
 # the terminal. Should work in all bash shell. 
 # http://unix.stackexchange.com/questions/38072/how-can-i-save-the-last-command-to-a-file
